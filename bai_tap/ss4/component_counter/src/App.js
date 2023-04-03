7e17e32cfc8680d93e98d9614c7120bd8daf480e
dinhminhthong <dinhminhthong14022001@gmail.com>
2023-04-03 13:48:22 +0700

    c

---
+++ bai_tap/ss4/component_counter/src/App.js
@@ -0,0 +1,16 @@
+import Counter1 from "./component/Counter1";
+import Counter2 from "./component/Counter2";
+
+function App() {
+    return (
+        <div>
+            <tr>
+              <td><Counter1/></td>
+                <td><Counter2/></td>
+            </tr>
+
+        </div>
+    )
+}
+
+export default App;
---
+++ bai_tap/ss4/component_counter/src/component/Counter1.js
@@ -0,0 +1,16 @@
+import {useState} from "react";
+
+function Counter() {
+    const [counter, setCounter] = useState(1)
+    const handleIncrease = () => {
+        setCounter(counter + 1)
+    }
+    return (
+        <div className="Counter" style={{padding: 20}}>
+            <h1>{counter}</h1>
+            <button onClick={handleIncrease}>Increase</button>
+        </div>
+    );
+}
+
+export default Counter;
