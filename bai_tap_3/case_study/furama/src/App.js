2ae1cf04329ae8442c6a103c05dac10948af0ae1
dinhminhthong <dinhminhthong14022001@gmail.com>
2023-06-09 14:11:16 +0700

    ss

--- bai_tap_3/case_study/furama/src/App.js
+++ bai_tap_3/case_study/furama/src/App.js
@@ -3,16 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import FacilitiesList from "./component/facility/FacilitiesList";
-import CustomerList from "./component/customer/CustomerList";
 import Home from "./component/home/Home";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
-import ContractList from "./component/contract/ContractList";
 import FacilitiesEditForm from "./component/facility/FacilitiesEditForm";
 import FacilitiesAddForm from "./component/facility/FacilitiesAddForm";
-import CustomerEditForm from "./component/customer/CustomerEditForm";
-import CustomerAddForm from "./component/customer/CustomerAddForm";
-import ContractAddForm from "./component/facility/ContractAddForm";
+
 import ScrollToTop from "./component/ScrollToTop";
 
 function App() {
@@ -25,7 +21,7 @@ function App() {
           <Route path="/facility" element={<FacilitiesList />} />
           <Route path="/facility-edit/:id" element={<FacilitiesEditForm />} />
           <Route path="/facility-add" element={<FacilitiesAddForm />} />
-         
+
         </Routes>
         <Footer />
         <ToastContainer />
