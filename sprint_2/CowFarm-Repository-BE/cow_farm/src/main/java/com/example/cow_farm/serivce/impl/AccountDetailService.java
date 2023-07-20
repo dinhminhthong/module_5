365bae83aca26b7b3f04dc5d63c4b7406fc26ac8
dinhminhthong <dinhminhthong14022001@gmail.com>
2023-07-20 08:46:00 +0700

    erd

--- sprint_2/CowFarm-Repository-BE/cow_farm/src/main/java/com/example/cow_farm/serivce/impl/AccountDetailService.java
+++ sprint_2/CowFarm-Repository-BE/cow_farm/src/main/java/com/example/cow_farm/serivce/impl/AccountDetailService.java
@@ -10,11 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- * Created by: Phạm Tiến
- * Date: 29/03/2023
- * Class: AccountDetailService
- */
+
 @Service
 public class AccountDetailService implements UserDetailsService {
 
